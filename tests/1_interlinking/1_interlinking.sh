d83cce55ecb0869d72dff95092b9b138bd318436
Hagen Fuchs <code@hfuchs.net>
2014-07-19 18:38:35 +0200

    tests/1_interlinking: Report line number after every test.
    I strongly suspect, my interlinking tests would've not catched
    regressions as expected.

--- tests/1_interlinking/1_interlinking.sh
+++ tests/1_interlinking/1_interlinking.sh
@@ -1,7 +1,10 @@
 # (Gotta give 'em a bit of time so I can watch if necessary!)
 tmux send-keys -t $testname:0 "vim -u vimrc.tests $testdir/$testname.wt" C-m
 sleep 2
-tmux send-keys -t $testname:0 "5G,,j7G,,j10G,,j13G,,j"
+tmux send-keys -t $testname:0 "5G,,j" C-g "7G,,j" C-g "10G,,j" C-g "13G,,j" C-g
+sleep 2
+# 2014-07-19, New tests after the break.
+tmux send-keys -t $testname:0 "17Gf>,,j" C-g
 sleep 2
 tmux send-keys -t $testname:0 ":messages" C-m
 sleep 2
